304581fbd9ec3e95f479ed8cbff1bab9245e001b
Ivan <vanfretes@gmail.com>
2023-08-15 23:06:14 -0400

    feat: add test for create and get a category name

--- app/offert/tests.py
+++ app/offert/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
-
+from .models import OffertCategory
 # Create your tests here.
-class BusinessModelTest(TestCase):
-    def test_only_for_porpuse(self):
-        self.assertFalse(True)
+class OffertCategoryModelTest(TestCase):
+   def setUp(self):
+      self.category_created = OffertCategory(name='test_1')
+      self.category_created.save()
+
+   def test_get_by_name(self):
+      self.assertEqual(self.category_created.name, 'test_1')
+
+   def test_failed_by_name(self):
+      self.assertIsNot(self.category_created.name, 'test_2')
+
+   def tearDown(self):
+        self.category_created.delete()
