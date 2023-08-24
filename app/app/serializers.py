c9c62f273102354f0c10cf671851cd2c9bd3ac3f
Ivan <vanfretes@gmail.com>
2023-08-24 12:49:44 -0400

    feat: reorganice routes

--- app/app/serializers.py
+++ app/app/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import routers, serializers, viewsets, permissions
+from rest_framework import serializers, viewsets, permissions
 from django.contrib.auth.models import User, Group
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,12 +13,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         model = Group
         fields = ['url', 'name']
 
-# ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
